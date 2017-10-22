eb0296a9048afe995890c01606dfd01889cfbd01
Cristhian Fuertes <cristhian.fuertes@alertlogic.com>
2017-10-21 19:23:00 -0500

    First rendering of the page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from '../components/toolbar/toolbar.component';
 import { SearchComponent } from '../components/search/search.component';
 import { HomeComponent } from '../components/home/home.component';
+import { FooterComponent } from '../components/footer/footer.component';
 import { MarvelAPIService } from '../services/marvel-api.service';
 import { BASE_URL, PUBLIC_KEY, API_VERSION } from './providers';
-import { FooterComponent } from '../components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    FooterComponent,
+    ToolbarComponent
   ],
   imports: [
+    MaterialModule,
     HttpModule,
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(ROUTES, { useHash: true })
   ],
   providers: [
               { provide: BASE_URL, useValue: 'https://gateway.marvel.com'},
